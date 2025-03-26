6478389208a763cbab85f08e0c80918eff41f653
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-26 18:25:07 -0400

    depoloy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NETLIFY_URL ||  "https://kambaz-node-server-app-2s25.onrender.com",
+        origin: process.env.NETLIFY_URL ||  "https://kambaz-node-server-app-ffcv.onrender.com",
     })
 );
 const sessionOptions = {
