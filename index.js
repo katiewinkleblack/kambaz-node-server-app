7975f0971b97f8d31a048738385252e4e23d6837
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-26 13:57:51 -0400

    m

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(
     cors({
         credentials: true,
-        origin: [ process.env.NETLIFY_URL,  "https://kambaz-node-server-app-r70q.onrender.com"],
+        origin: process.env.NETLIFY_URL ||  "https://kambaz-node-server-app-r70q.onrender.com",
     })
 );
 const sessionOptions = {
