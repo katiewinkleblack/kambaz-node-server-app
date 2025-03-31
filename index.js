11acb9d6f0bc376eea4e1ead323e768e495bb365
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-31 10:09:22 -0400

    node

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
-
-
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
 
 app.use(session(sessionOptions));
 app.use(express.json());
